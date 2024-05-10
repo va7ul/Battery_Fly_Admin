3821dbf0dcb4ec849b17a1e970662e00338e3d6e
Va7ul <vas7ul@gmail.com>
2024-05-10 19:50:57 +0300

    Create ProfileButton.styled.js

---
+++ src/components/Header/ProfileButton/ProfileButton.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 10px;
+  padding: 7px;
+  min-width: 112px;
+  gap: 6px;
+  background-color: ${props => props.theme.colors.secondary};
+  transition: ${props => props.theme.transition.main};
+
+  &:hover {
+    background-color: ${props => props.theme.colors.hoverColor};
+  }
+`;
