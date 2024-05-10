bc5db126cc5de28bbf35af9d48d3e8942b9b608e
Va7ul <vas7ul@gmail.com>
2024-05-10 19:48:39 +0300

    Create Layout.styled.js

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { hidden } from '../../styles/GlobalStyled';
+
+export const Container = styled.div`
+  max-width: 360px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1280px) {
+    max-width: 1280px;
+  }
+`;
+
+export const AppTitle = styled.h1`
+  ${hidden}
+`;
