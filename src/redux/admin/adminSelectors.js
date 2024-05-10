3bb5eafc4193689cfa1c437afb0b7087cb4ac92b
Va7ul <vas7ul@gmail.com>
2024-05-10 19:47:30 +0300

    Create adminSelectors.js

---
+++ src/redux/admin/adminSelectors.js
@@ -0,0 +1,5 @@
+export const selectLogin = state => state.admin.login;
+export const selectIsLoggedIn = state => state.admin.isLoggedIn;
+export const selectIsRefreshing = state => state.admin.isRefreshing;
+export const selectIsLoading = state => state.admin.isLoading;
+export const selectErrorStatus = state => state.admin.errorStatus;
