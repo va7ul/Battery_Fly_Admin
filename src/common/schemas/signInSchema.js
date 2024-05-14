311c0ae46d5e2d642eb17ed3bca8be8ccc99ec47
Va7ul <vas7ul@gmail.com>
2024-05-14 19:10:56 +0300

    connect admin routes

--- src/common/schemas/signInSchema.js
+++ src/common/schemas/signInSchema.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const signInSchema = Yup.object().shape({
-  email: Yup.string().required('Введіть свій логін, будь ласка'),
+  login: Yup.string().required('Введіть свій логін, будь ласка'),
   password: Yup.string()
     .min(8, 'Пароль має бути не менший 8 символів')
     .max(20, 'Пароль має бути не довший 20 символів')
