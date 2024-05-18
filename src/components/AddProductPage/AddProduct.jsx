8bae2c458b4cd6fde01c6dc1251c98b30a931fca
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-05-18 23:52:17 +0300

    added_3 fields

--- src/components/AddProductPage/AddProduct.jsx
+++ src/components/AddProductPage/AddProduct.jsx
@@ -1,8 +1,55 @@
+import { Formik } from 'formik';
+import { StyledForm, Title, Label, Box, StyledField } from "./AddProduct.styled";
 
 
-export const AddProduct = () => { 
+export const AddProduct = () => {
 
     return (
-<>AddProduct</>
-)
-}
+     <div>
+            <Formik
+                initialValues={{
+                    // firstName: firstName,
+                    // lastName: lastName,
+                    // patronymic: patronymic,
+                }}
+                // validationSchema={userDataSchema}
+                // onSubmit={(values, _) => {
+                //     const userData = {
+                //         firstName: values.firstName,
+                //         lastName: values.lastName,
+                //         patronymic: values.patronymic,
+                //         tel: formikTel,
+                //     };
+                // }}
+            >
+                
+                <StyledForm>
+                    <Title>Додати товар</Title>
+                    <Label>
+                        Назва товару
+                        <Box>
+                    <StyledField name="name" type="text" />
+                    {/* <StyledErrorMessage name="firstName" component="div" /> */}
+                </Box>
+                    </Label>
+
+                    <Label>
+                        Ціна за одиницю
+                        <Box>
+                    <StyledField name="price" type="number" />
+                    {/* <StyledErrorMessage name="lastName" component="div" /> */}
+                </Box>
+                    </Label>
+
+                    <Label>
+                        Повний опис
+                        <Box>
+                    <StyledField name="description" type="text" />
+                    {/* <StyledErrorMessage name="patronymic" component="div" /> */}
+                </Box>
+                    </Label>
+                </StyledForm>
+            </Formik>
+            </div>
+    )
+};
