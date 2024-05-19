6eae5154816244a6b938480dde104e685598e92b
Va7ul <vas7ul@gmail.com>
2024-05-19 22:22:39 +0300

    create productsTable

---
+++ src/components/Shared/ProductsTable/ProductsTable.jsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import { selectProducts } from '../../../redux/products/productsSelectors';
+import Box from '@mui/material/Box';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import {
+  DataGrid,
+  GridToolbar,
+  GridActionsCellItem,
+  useGridApiRef,
+} from '@mui/x-data-grid';
+
+export const ProductsTable = () => {
+  const apiRef = useGridApiRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const products = useSelector(selectProducts);
+
+  const autosizeOptions = useMemo(
+    () => ({
+      includeHeaders: true,
+      includeOutliers: true,
+      expand: true,
+    }),
+    []
+  );
+
+  const rows = useMemo(
+    () =>
+      products.map(el => ({
+        id: el.codeOfGood,
+        codeOfGood: el.codeOfGood,
+        name: el.name,
+        category: `${el.category} ${el.type ? el.type : ''}`,
+        quantity: el.quantity,
+        price: el.price,
+        discount: el.discount,
+        popular: el.popular,
+      })),
+    [products]
+  );
+
+  useEffect(() => {
+    if (apiRef.current) {
+      apiRef.current.autosizeColumns(autosizeOptions);
+    }
+  }, [apiRef, autosizeOptions, products]);
+
+  const columns = [
+    {
+      field: 'codeOfGood',
+      headerName: 'Код товару',
+      type: 'number',
+      align: 'left',
+      headerAlign: 'left',
+    },
+    { field: 'name', headerName: 'Назва товару' },
+    {
+      field: 'category',
+      headerName: 'Категорія та тип',
+    },
+    {
+      field: 'quantity',
+      headerName: 'Кількість',
+      type: 'number',
+      align: 'left',
+      headerAlign: 'left',
+    },
+    {
+      field: 'price',
+      headerName: 'Ціна',
+      type: 'number',
+      align: 'left',
+      headerAlign: 'left',
+    },
+    {
+      field: 'discount',
+      headerName: 'Знижка',
+      type: 'number',
+      align: 'left',
+      headerAlign: 'left',
+      valueFormatter: value => {
+        if (value == null) {
+          return '';
+        }
+        return `${value.toLocaleString()} %`;
+      },
+    },
+    {
+      field: 'popular',
+      headerName: 'Популярний',
+      type: 'boolean',
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: '',
+      width: 150,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<EditOutlinedIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  const handleEditClick = id => () => {
+    navigate(`/admin/assortment/${id}`);
+  };
+
+  const handleDeleteClick = id => () => {
+    // setRows(rows.filter(row => row.id !== id));
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        maxWidth: '100%',
+      }}
+    >
+      <DataGrid
+        apiRef={apiRef}
+        rows={rows}
+        columns={columns}
+        autosizeOptions={autosizeOptions}
+        pageSizeOptions={[10, 25, 100]}
+        hideFooterSelectedRowCount
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+          },
+        }}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 10 } },
+        }}
+      />
+    </Box>
+  );
+};
---
+++ src/components/Shared/ProductsTable/ProductsTable.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  left: 0;
+  top: 0;
+  display: flex;
+  margin: 0 auto;
+  z-index: 3;
+
+  background-color: ${props => props.theme.colors.greyBackgroundPaper};
+  box-shadow: 0px 4px 4px -2px rgba(0, 0, 0, 0.25);
+
+  align-items: center;
+  max-width: 1280px;
+  padding: 0 110px;
+`;
+
+export const DesktopWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  gap: 100px;
+`;
