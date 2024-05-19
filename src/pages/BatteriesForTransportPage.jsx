366e6b5b72225edf32f2a865afb5a58a9005e860
Va7ul <vas7ul@gmail.com>
2024-05-19 17:37:34 +0300

    Create BatteriesForTransportPage.jsx

---
+++ src/pages/BatteriesForTransportPage.jsx
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getBatteriesForTransport } from '../redux/products/productsOperations';
+import { useLoader } from 'utils/hooks';
+import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { ProductsTable } from 'components/Shared/ProductsTable/ProductsTable';
+
+const BatteriesForTransportPage = () => {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useLoader();
+
+  useEffect(() => {
+    dispatch(getBatteriesForTransport());
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoading && <CustomLoader />}
+      {!isLoading && !error && (
+        <ProductsTable category={'Батареї для електротранспорту'} />
+      )}
+    </>
+  );
+};
+
+export default BatteriesForTransportPage;
