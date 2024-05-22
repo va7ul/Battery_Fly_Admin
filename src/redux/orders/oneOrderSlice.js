0c7291a752845d9393fe352a245281a9c9cabd57
Va7ul <vas7ul@gmail.com>
2024-05-22 14:03:59 +0300

    Create oneOrderSlice.js

---
+++ src/redux/orders/oneOrderSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getOneOrder } from './ordersOperations';
+
+const initialState = {
+  result: {
+    _id: '660c0fe817a24c70c02f36a3',
+    status: 'Нове',
+    numberOfOrder: '100119',
+    firstName: 'Vasyl',
+    lastName: 'Podvirnyi',
+    email: 'test@gmail.com',
+    comment: '',
+    tel: '+380962985515',
+    total: 1016,
+    promoCode: '',
+    promoCodeDiscount: 0,
+    discountValue: 0,
+    together: 1016,
+    cartItems: [{}, {}],
+    delivery: 'Нова пошта',
+    city: 'Ворзель',
+    warehouse: 'Відділення №2 (до 10 кг): вул. Курортна, 23а',
+    payment: 'Картою по реквізитах фізичних осіб',
+    createdAt: '2024-04-02T14:02:16.429+00:00',
+  },
+
+  isLoading: false,
+  error: null,
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.result = action.payload.result;
+};
+
+const oneOrderSlice = createSlice({
+  name: 'orders',
+  initialState,
+  reducers: {},
+  extraReducers: builder =>
+    builder
+      .addCase(getOneOrder.pending, handlePending)
+      .addCase(getOneOrder.rejected, handleRejected)
+      .addCase(getOneOrder.fulfilled, handleFulfilled),
+});
+
+export const oneOrderReducer = oneOrderSlice.reducer;
