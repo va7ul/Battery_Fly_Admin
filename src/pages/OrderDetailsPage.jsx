5a43f4aa0e1d1753ddc851408eadae6925dfe7d9
Va7ul <vas7ul@gmail.com>
2024-05-24 18:57:41 +0300

    Update OrderDetailsPage.jsx

--- src/pages/OrderDetailsPage.jsx
+++ src/pages/OrderDetailsPage.jsx
@@ -1,26 +1,26 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getOneOrder } from '../redux/orders/ordersOperations';
-import {
-  selectOneOrderIsLoading,
-  selectOneOrderError,
-} from '../redux/orders/ordersSelectors';
-import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getOneOrder } from '../redux/orders/ordersOperations';
+// import {
+//   selectOneOrderIsLoading,
+//   selectOneOrderError,
+// } from '../redux/orders/ordersSelectors';
+// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
 import { OrderDetails } from 'components/OrderDetailsPage/OrderDetails';
 
 const OrderDetailsPage = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectOneOrderIsLoading);
-  const error = useSelector(selectOneOrderError);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectOneOrderIsLoading);
+  // const error = useSelector(selectOneOrderError);
 
-  useEffect(() => {
-    dispatch(getOneOrder());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getOneOrder());
+  // }, [dispatch]);
 
   return (
     <>
       {/* {isLoading && <CustomLoader />}
-      {!isLoading && !error && <OrdersTable />} */}
+      {!isLoading && !error && <OrderDetails />} */}
       <OrderDetails />
     </>
   );
