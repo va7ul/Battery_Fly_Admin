851d90660db2b06bd654a427c48e5d5266e42498
Va7ul <vas7ul@gmail.com>
2024-05-24 18:51:15 +0300

    Create ApplicationsPage.jsx

---
+++ src/pages/ApplicationsPage.jsx
@@ -0,0 +1,29 @@
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getApplications } from '../redux/orders/ordersOperations';
+// import {
+//   selectApplicationsIsLoading,
+//   selectApplicationsError,
+// } from '../redux/orders/ordersSelectors';
+// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { ApplicationsTable } from 'components/ApplicationsPage/ApplicationsTable';
+
+const ApplicationsPage = () => {
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectAllOrdersIsLoading);
+  // const error = useSelector(selectAllOrdersError);
+
+  // useEffect(() => {
+  //   dispatch(getApplications());
+  // }, [dispatch]);
+
+  return (
+    <>
+      {/* {isLoading && <CustomLoader />}
+      {!isLoading && !error && <ApplicationsTable />} */}
+      <ApplicationsTable />
+    </>
+  );
+};
+
+export default ApplicationsPage;
