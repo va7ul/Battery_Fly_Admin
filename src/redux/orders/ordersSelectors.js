636fc40bbc2e1e987f5e8c3243389dffb8e351df
Va7ul <vas7ul@gmail.com>
2024-05-24 18:51:01 +0300

    add operations for new routes

--- src/redux/orders/ordersSelectors.js
+++ src/redux/orders/ordersSelectors.js
@@ -6,3 +6,6 @@ export const selectAllOrdersIsLoading = state =>
   state.orders.ordersList.isLoading;
 export const selectAllOrdersError = state => state.orders.ordersList.error;
 export const selectAllOrders = state => state.orders.ordersList.allOrders;
+export const selectPrins3D = state => state.orders.ordersList.prins3D;
+export const selectQuickOrders = state => state.orders.ordersList.quickOrders;
+export const selectApplications = state => state.orders.ordersList.applications;
