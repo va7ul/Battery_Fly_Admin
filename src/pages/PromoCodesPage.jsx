cf07d9eba1c2264226e2e4ae87e7122a2788603d
Va7ul <vas7ul@gmail.com>
2024-05-25 20:35:37 +0300

    add promoCode component

--- src/pages/PromoCodesPage.jsx
+++ src/pages/PromoCodesPage.jsx
@@ -1,5 +1,29 @@
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getPromoCodes } from '../redux/promoCodes/promoCodesOperations';
+// import {
+//   selectPromoCodesIsLoading,
+//   selectPromoCodesError,
+// } from '../redux/promoCodes/promoCodesSelectors';
+// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { PromoCodesTable } from 'components/PromoCodesPage/PromoCodesTable';
+
 const PromoCodesPage = () => {
-  return <>PromoCodesPage</>;
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectPromoCodesIsLoading);
+  // const error = useSelector(selectPromoCodesError);
+
+  // useEffect(() => {
+  //   dispatch(getPromoCodes());
+  // }, [dispatch]);
+
+  return (
+    <>
+      {/* {isLoading && <CustomLoader />}
+      {!isLoading && !error && <PromoCodesTable />} */}
+      <PromoCodesTable />
+    </>
+  );
 };
 
 export default PromoCodesPage;
