c2b0796cef6b1c403269727d1a69a1cbd81b2b44
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-05-29 18:24:33 +0300

    created_ EditProduct

---
+++ src/components/EditProductPage/EditProduct.jsx
@@ -0,0 +1,235 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Formik } from 'formik';
+import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { yellow } from '@mui/material/colors';
+import { addProduct } from '../../redux/products/productsOperations';
+import { selectOneProduct } from '../../redux/products/productsSelectors';
+import { productSchema } from '../../common/schemas/productSchema'
+import { Container, StyledForm, Title, SubTitle, Label, Box, StyledField, Input, StyledTextField, SubmitButton, StyledErrorMessage } from "../AddProductPage/AddProduct/AddProduct.styled";
+
+export const EditProduct = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [images, setImages] = useState([]);
+    // const [sale, setSale] = useState(false);
+    // const [popular, setPopular] = useState(false);
+
+    const {name, description, image, price, quantity, sale, popular, category, type, information, discount} = useSelector(selectOneProduct)
+
+    const attachImages = e => {
+        setImages(e.currentTarget.files);
+    };
+
+    const changeType = () => {
+        if (type === 'null') {
+            return category;
+        }
+    };
+
+    const AddProductButton = () => {
+        navigate(
+            `/admin/assortment/batteries-${type}`
+        );
+    };
+
+    return (
+        <Container>
+            <Formik
+                initialValues={{
+                    name: name,
+                    price: price,
+                    description: description,
+                    quantity: quantity,
+                    sale: sale,
+                    discount: discount,
+                    information: information,
+                    category: category,
+                    type: type,
+                    popular: popular,
+                    image: image,
+
+                }}
+                enctype="multipart/form-data"
+                validationSchema={productSchema}
+                onSubmit={values => {
+                    const formData = new FormData();
+                    formData.append('name', values.name);
+                    formData.append('price', values.price);
+                    formData.append('description', values.description);
+                    formData.append('quantity', values.quantity);
+                    formData.append('sale', values.sale);
+                    formData.append('discount', values.discount || 10);
+                    formData.append('category', values.category);
+                    formData.append('type', values.type = changeType() || type);
+                    formData.append('popular', values.popular);
+                    formData.append('information', values.information);
+
+                    for (const image of images) {
+                        formData.append('files', image)
+                    };
+                    
+                    dispatch(addProduct(formData)).then(result => {
+                        if (result.meta.requestStatus === 'fulfilled') {
+                            AddProductButton();
+                        }
+                    })
+                }}
+            >
+                
+                <StyledForm>
+                    <Title>Додавання товару</Title>
+                    <Label>
+                        Назва товару
+                        <Box>
+                            <StyledField name="name" type="text" />
+                            <StyledErrorMessage name="name" component="div" />
+                        </Box>
+                    </Label>
+
+                    <Label>
+                        Ціна за одиницю
+                        <Box>
+                            <StyledField name="price" type="number" />
+                            <StyledErrorMessage name="price" component="div" />
+                        </Box>
+                    </Label>
+
+                    <Label>
+                        Повний опис
+                        <Box>
+                            <StyledTextField name="description" type="text" component="textarea" placeholder="Наприкінці кожного пункту ОБОВ'ЯЗКОВО ставте &#171;;&#187;, крім останнього!" />
+                            <StyledErrorMessage name="description" component="div" />
+                        </Box>
+                    </Label>
+                    <Box>
+                        <SubTitle>Додати фото</SubTitle>
+                        
+                        <Input
+                            accept="image/*"
+                            type="file"
+                            name="files"
+                            onChange={attachImages}
+                            multiple
+                        />
+                    </Box>
+                    <Label>
+                        Кількість в наявності
+                        <Box>
+                            <StyledField name="quantity" type="number" />
+                            <StyledErrorMessage name="quantity" component="div" />
+                        </Box>
+                    </Label>
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label"
+                            sx={{
+                                color: '#1f1f1f',
+                                fontSize: '16px',
+                                fontWeight: '600',
+                                '&.Mui-focused': {
+                                    color: '#1f1f1f',
+                                }
+                            }}
+                        >Знижка</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                        >
+                            <FormControlLabel
+                                value="yes"
+                                // onChange={() => { setSale(true) }}
+                                control={<Radio sx={{
+                                    '&.Mui-checked': {
+                                        color: yellow[800],
+                                    },
+                                }} />} label="Так" />
+                            <FormControlLabel
+                                value="no"
+                                // onChange={() => { setSale(false) }}
+                                control={<Radio sx={{
+                                    '&.Mui-checked': {
+                                        color: yellow[800],
+                                    },
+                                }} />} label="Ні" />
+                        </RadioGroup>
+                    </FormControl>
+                    {sale && <Label>
+                        Відсоток знижки
+                        <Box>
+                            <StyledField name="discount" type="number" />
+                            <StyledErrorMessage name="discount" component="div" />
+                        </Box>
+                    </Label>}
+                  
+                    <Label>
+                        Категорія
+                        <Box>
+                            <StyledField name="category" type="text" value={category} />
+                        </Box>
+                    </Label>
+
+                    {type !== "null" && <Label>
+                        Тип
+                        <Box>
+                            <StyledField name="type" type="text" value={type} />
+                        </Box>
+                    </Label>}
+                     
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label"
+                            sx={{
+                                color: '#1f1f1f',
+                                fontSize: '16px',
+                                fontWeight: '600',
+                                '&.Mui-focused': {
+                                    color: '#1f1f1f',
+                                }
+                            }}
+                        >Популярний</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                        >
+                            <FormControlLabel
+                                value="yes"
+                                // onChange={() => { setPopular(true) }}
+                                control={<Radio sx={{
+                                    '&.Mui-checked': {
+                                        color: yellow[800],
+                                    },
+                                }} />} label="Так" />
+                            <FormControlLabel
+                                value="no"
+                                // onChange={() => { setPopular(false) }}
+                                control={<Radio sx={{
+                                    '&.Mui-checked': {
+                                        color: yellow[800],
+                                    },
+                                }} />} label="Ні" />
+                        </RadioGroup>
+                    </FormControl>
+                    <Label>
+                        Інформація
+                        <Box>
+                            <StyledTextField name="information" type="text" component="textarea" />
+                            <StyledErrorMessage name="information" component="div" />
+                        </Box>
+                    </Label>
+                    <SubmitButton
+                        type="submit"
+                    >
+                        Додати товар
+                    </SubmitButton>
+                </StyledForm>
+            </Formik>
+        </Container>
+    );
+};
