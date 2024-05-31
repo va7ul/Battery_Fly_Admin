d81fefb8c5536d975e67f0ce7a3a4dedf63e630d
Va7ul <vas7ul@gmail.com>
2024-05-31 23:56:29 +0300

    Update QuickOrdersPage.jsx

--- src/pages/QuickOrdersPage.jsx
+++ src/pages/QuickOrdersPage.jsx
@@ -1,27 +1,26 @@
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getQuickOrders } from '../redux/orders/ordersOperations';
-// import {
-//   selectApplicationsIsLoading,
-//   selectApplicationsError,
-// } from '../redux/orders/ordersSelectors';
-// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getQuickOrders } from '../redux/orders/ordersOperations';
+import {
+  selectAllOrdersIsLoading,
+  // selectAllOrdersError,
+} from '../redux/orders/ordersSelectors';
+import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
 import { QuickOrdersTable } from 'components/QuickOrdersPage/QuickOrdersTable';
 
 const QuickOrdersPage = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectAllOrdersIsLoading);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAllOrdersIsLoading);
   // const error = useSelector(selectAllOrdersError);
 
-  // useEffect(() => {
-  //   dispatch(getQuickOrders());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(getQuickOrders());
+  }, [dispatch]);
 
   return (
     <>
-      {/* {isLoading && <CustomLoader />}
-      {!isLoading && !error && <QuickOrdersTable />} */}
-      <QuickOrdersTable />
+      {isLoading && <CustomLoader />}
+      {!isLoading  && <QuickOrdersTable />}
     </>
   );
 };
