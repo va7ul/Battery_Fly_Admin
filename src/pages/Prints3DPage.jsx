71d7394aaf08b18fb7fb6f502807fabc3c8405a2
Va7ul <vas7ul@gmail.com>
2024-05-31 23:56:25 +0300

    Update Prints3DPage.jsx

--- src/pages/Prints3DPage.jsx
+++ src/pages/Prints3DPage.jsx
@@ -1,27 +1,26 @@
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getPrints3D } from '../redux/orders/ordersOperations';
-// import {
-//   selectApplicationsIsLoading,
-//   selectApplicationsError,
-// } from '../redux/orders/ordersSelectors';
-// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPrints3D } from '../redux/orders/ordersOperations';
+import {
+  selectAllOrdersIsLoading,
+  selectAllOrdersError,
+} from '../redux/orders/ordersSelectors';
+import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
 import { Prints3DTable } from 'components/Prints3DPage/Prints3DTable';
 
 const Prins3DPage = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectAllOrdersIsLoading);
-  // const error = useSelector(selectAllOrdersError);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAllOrdersIsLoading);
+  const error = useSelector(selectAllOrdersError);
 
-  // useEffect(() => {
-  //   dispatch(getPrints3D());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPrints3D());
+  }, [dispatch]);
 
   return (
     <>
-      {/* {isLoading && <CustomLoader />}
-      {!isLoading && !error && <Prints3DTable />} */}
-      <Prints3DTable />
+      {isLoading && <CustomLoader />}
+      {!isLoading && !error && <Prints3DTable />}
     </>
   );
 };
