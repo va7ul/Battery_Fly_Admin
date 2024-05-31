8b1ce6251767dc9a63ccaf92f347ddbaa5a3063a
Va7ul <vas7ul@gmail.com>
2024-06-01 00:01:32 +0300

    Update PromoCodesPage.jsx

--- src/pages/PromoCodesPage.jsx
+++ src/pages/PromoCodesPage.jsx
@@ -1,27 +1,26 @@
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getPromoCodes } from '../redux/promoCodes/promoCodesOperations';
-// import {
-//   selectPromoCodesIsLoading,
-//   selectPromoCodesError,
-// } from '../redux/promoCodes/promoCodesSelectors';
-// import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPromoCodes } from '../redux/promoCodes/promoCodesOperations';
+import {
+  selectPromoCodesIsLoading,
+  // selectPromoCodesError,
+} from '../redux/promoCodes/promoCodesSelectors';
+import { CustomLoader } from 'components/Shared/CustomLoader/CustomLoader';
 import { PromoCodesTable } from 'components/PromoCodesPage/PromoCodesTable';
 
 const PromoCodesPage = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectPromoCodesIsLoading);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectPromoCodesIsLoading);
   // const error = useSelector(selectPromoCodesError);
 
-  // useEffect(() => {
-  //   dispatch(getPromoCodes());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPromoCodes());
+  }, [dispatch]);
 
   return (
     <>
-      {/* {isLoading && <CustomLoader />}
-      {!isLoading && !error && <PromoCodesTable />} */}
-      <PromoCodesTable />
+      {isLoading && <CustomLoader />}
+      {!isLoading && <PromoCodesTable />}
     </>
   );
 };
