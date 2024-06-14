b65fa58286d566b7bf89581d9b5b4056e297ff1a
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-06-14 17:34:40 +0300

    added_0_to_productSchema

--- src/common/schemas/productSchema.js
+++ src/common/schemas/productSchema.js
@@ -9,7 +9,7 @@ export const productSchema = Yup.object().shape({
   description: Yup.string().required('Введіть опис товару'),
   quantity: Yup.number()
     .required('Введіть кількість товару')
-    .positive('Число повинно бути позитивним')
+    .min(0, 'Число повинно бути більшим або дорівнювати 0')
     .integer(),
   discount: Yup.number().positive('Число повинно бути позитивним').integer(),
   information: Yup.string().required('Введіть інформацію про товар'),
