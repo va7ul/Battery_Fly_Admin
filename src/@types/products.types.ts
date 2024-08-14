ca42fad88eb034fe4d5c5dc0ad8c74d36bbc49df
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-08-14 21:34:14 +0300

    added_properties

--- src/@types/products.types.ts
+++ src/@types/products.types.ts
@@ -27,15 +27,69 @@ export type Product = {
   updatedAt: string;
   discount: number;
 };
- 
+
+// export type oneProduct = {
+//   result: {
+// _id: string;
+//   codeOfGood: string;
+//   name: string;
+//   description: string;
+//   image: string[];
+//   price: number | string;
+//   quantity: number;
+//   sale: boolean;
+//   popular: boolean;
+//   category: string;
+//   type?: string;
+//   capacity?: BatteryConfig;
+//   holder?: boolean;
+//   information: string;
+//   createdAt: string;
+//   updatedAt: string;
+//     discount: number;
+//   priceOneProduct?: number
+//   }
+  
+// };
+
+// export type Result1 = {
+//   _id?: string;
+//   codeOfGood?: string;
+//   name?: string;
+//   quantity?: number;
+//   sale?: boolean;
+//   popular?: boolean;
+//   type?: string;
+//   holder?: boolean;
+//   createdAt?: string;
+//   updatedAt?: string;
+//   discount?: number;
+//   description: string;
+//   capacity?: BatteryConfig;
+//   capacityKey?: string;
+//   information: string;
+//   price: string | number;
+//   priceOneProduct: string | number;
+//   image: string[];
+// };
+
 export type Result = {
+  codeOfGood: string;
+  name: string;
+  quantity: number;
+  sale: boolean;
+  popular: boolean;
+  type: string;
+  discount: number;
   description: string;
   capacity?: BatteryConfig;
   capacityKey?: string;
+  holder: boolean;
   information: string;
   price: string | number;
   priceOneProduct: string | number;
-  image: string[];
+  image: string[] | File[];
+  category: string;
 };
 
 export type AddProduct = {
