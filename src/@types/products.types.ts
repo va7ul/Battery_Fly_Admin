55c54871a0c2150b5e41f74e65bc0044f8f863d7
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-08-14 22:26:27 +0300

    added_types

--- src/@types/products.types.ts
+++ src/@types/products.types.ts
@@ -28,51 +28,6 @@ export type Product = {
   discount: number;
 };
 
-// export type oneProduct = {
-//   result: {
-// _id: string;
-//   codeOfGood: string;
-//   name: string;
-//   description: string;
-//   image: string[];
-//   price: number | string;
-//   quantity: number;
-//   sale: boolean;
-//   popular: boolean;
-//   category: string;
-//   type?: string;
-//   capacity?: BatteryConfig;
-//   holder?: boolean;
-//   information: string;
-//   createdAt: string;
-//   updatedAt: string;
-//     discount: number;
-//   priceOneProduct?: number
-//   }
-  
-// };
-
-// export type Result1 = {
-//   _id?: string;
-//   codeOfGood?: string;
-//   name?: string;
-//   quantity?: number;
-//   sale?: boolean;
-//   popular?: boolean;
-//   type?: string;
-//   holder?: boolean;
-//   createdAt?: string;
-//   updatedAt?: string;
-//   discount?: number;
-//   description: string;
-//   capacity?: BatteryConfig;
-//   capacityKey?: string;
-//   information: string;
-//   price: string | number;
-//   priceOneProduct: string | number;
-//   image: string[];
-// };
-
 export type Result = {
   codeOfGood: string;
   name: string;
@@ -106,4 +61,15 @@ export type AddProduct = {
   holder?: boolean;
   information: string;
   discount: number;
+};
+
+export type CategoryMap = {
+  [key: string]: string;
+};
+
+export type CapacityObj = {
+  capacity: string;
+  description: string;
+  price: string;
+  holder: string;
 };
